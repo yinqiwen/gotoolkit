0fc0f10e04ab793ff326e3ada1898d2edfdcc08e
yinqiwen <yinqiwen@gmail.com>
2016-11-11 16:26:55 +0800

    update

--- es/es.go
+++ es/es.go
@@ -19,10 +19,18 @@ import (
 type DataSourceSetting struct {
 	Pattern string
 	Indice  string
-	Weight  int
-	Class   string
+	Type    string
+	CW1     int
+	C1      string //class1
+	C2      string //class2
 }
+
 type IndiceSetting struct {
+	Name   []string
+	Shards int
+}
+
+type TypeSetting struct {
 	Mapping map[string][]string
 }
 
@@ -34,6 +42,7 @@ func getMD5Hash(text string) string {
 
 type ESConfig struct {
 	Indice IndiceSetting
+	Type   TypeSetting
 	CP     []DataSourceSetting
 }
 
@@ -56,24 +65,23 @@ func main() {
 		return
 	}
 
-	for indice, props := range cfg.Indice.Mapping {
-		propsMapping := make(map[string]interface{})
-		for _, prop := range props {
-			propMap := make(map[string]string)
-			propMap["type"] = "string"
-			propMap["analyzer"] = "ik_syno"
-			propMap["search_analyzer"] = "ik_syno"
-			propMap["include_in_all"] = "true"
-			propsMapping[prop] = propMap
+	typeIndices := make(map[string][]string)
+	for _, cp := range cfg.CP {
+		indices := typeIndices[cp.Type]
+		found := false
+		for _, k := range indices {
+			if k == cp.Indice {
+				found = true
+				break
+			}
 		}
-		app := make(map[string]interface{})
-		app["properties"] = propsMapping
-		all := make(map[string]interface{})
-		all["enabled"] = false
-		app["_all"] = all
+		if !found {
+			indices = append(indices, cp.Indice)
+			typeIndices[cp.Type] = indices
+		}
+	}
 
-		// esMapping := make(map[string]interface{})
-		// esMapping["mappings"] = app
+	for _, indice := range cfg.Indice.Name {
 		esIndex := make(map[string]interface{})
 		esAnalyzer := make(map[string]interface{})
 		my_synonym := make(map[string]interface{})
@@ -92,9 +100,12 @@ func main() {
 		tmp["analysis"] = esIndex
 		settting := make(map[string]interface{})
 		settting["index"] = tmp
-		b, _ := json.Marshal(settting)
-		dest := *es + "/" + indice
+		if cfg.Indice.Shards > 0 {
+			settting["number_of_shards"] = cfg.Indice.Shards
+		}
 
+		b, _ = json.Marshal(settting)
+		dest := *es + "/" + indice
 		req, _ := http.NewRequest("PUT", dest, bytes.NewBuffer(b))
 		res, err := http.DefaultClient.Do(req)
 		if nil != err {
@@ -105,150 +116,49 @@ func main() {
 			log.Printf("###Update index for %v with %s  %s", indice, string(b), string(resb))
 			res.Body.Close()
 		}
-		dest = *es + "/" + indice + "/app/_mapping"
-		b, _ = json.Marshal(app)
-		req, _ = http.NewRequest("POST", dest, bytes.NewBuffer(b))
-		res, err = http.DefaultClient.Do(req)
-		if nil != err {
-			log.Printf("###Failed to update mapping with errror:%v", err)
-			return
-		} else {
-			resb, _ := ioutil.ReadAll(res.Body)
-			log.Printf("###Update mapping for %v with %s  %s", indice, string(b), string(resb))
-			res.Body.Close()
+
+	}
+	for t, props := range cfg.Type.Mapping {
+		propsMapping := make(map[string]interface{})
+		for _, prop := range props {
+			propMap := make(map[string]string)
+			propMap["type"] = "text"
+			propMap["analyzer"] = "ik_syno"
+			propMap["search_analyzer"] = "ik_syno"
+			propMap["include_in_all"] = "true"
+			propsMapping[prop] = propMap
+		}
+		c1 := make(map[string]interface{})
+		c1["type"] = "keyword"
+		propsMapping["c1"] = c1
+		c2 := make(map[string]interface{})
+		c2["type"] = "keyword"
+		propsMapping["c2"] = c2
+		app := make(map[string]interface{})
+		app["properties"] = propsMapping
+		all := make(map[string]interface{})
+		all["enabled"] = false
+		app["_all"] = all
+
+		// esMapping := make(map[string]interface{})
+		// esMapping["mappings"] = app
+
+		indices := typeIndices[t]
+		for _, indice := range indices {
+			dest := *es + "/" + indice + "/" + t + "/_mapping"
+			b, _ := json.Marshal(app)
+			req, _ := http.NewRequest("POST", dest, bytes.NewBuffer(b))
+			res, err := http.DefaultClient.Do(req)
+			if nil != err {
+				log.Printf("###Failed to update mapping with errror:%v", err)
+				return
+			} else {
+				resb, _ := ioutil.ReadAll(res.Body)
+				log.Printf("###Update mapping for %s/%v with %s  %s", indice, t, string(b), string(resb))
+				res.Body.Close()
+			}
 		}
 	}
-
-	// 	settings := make(map[string]indiceSetting)
-	// 	settings["default_data"] = indiceSetting{"default_data", `{
-	//     "mappings": {
-	//         "app": {
-	//             "_all": {
-	//                 "enabled": false
-	//             },
-	//             "properties": {
-	//                 "title": {
-	//                     "type": "string",
-	//                     "analyzer": "ik_max_word",
-	//                     "search_analyzer": "ik_max_word",
-	//                     "include_in_all": "true"
-	//                 },
-	// 				"desc": {
-	//                     "type": "string",
-	//                     "analyzer": "ik_max_word",
-	//                     "search_analyzer": "ik_max_word",
-	//                     "include_in_all": "true"
-	//                 },
-	// 				"content": {
-	//                     "type": "string",
-	//                     "analyzer": "ik_max_word",
-	//                     "search_analyzer": "ik_max_word",
-	//                     "include_in_all": "true"
-	//                 },
-	// 				"tag": {
-	//                     "type": "string",
-	//                     "analyzer": "ik_max_word",
-	//                     "search_analyzer": "ik_max_word",
-	//                     "include_in_all": "true"
-	//                 },
-	// 				"category": {
-	//                     "type": "string",
-	//                     "analyzer": "ik_max_word",
-	//                     "search_analyzer": "ik_max_word",
-	//                     "include_in_all": "true"
-	//                 },
-	// 				"seller": {
-	//                     "type": "string",
-	//                     "analyzer": "ik_max_word",
-	//                     "search_analyzer": "ik_max_word",
-	//                     "include_in_all": "true"
-	//                 },
-	// 				"director": {
-	//                     "type": "string",
-	//                     "analyzer": "ik_max_word",
-	//                     "search_analyzer": "ik_max_word",
-	//                     "include_in_all": "true"
-	//                 },
-	// 				"actor": {
-	//                     "type": "string",
-	//                     "analyzer": "ik_max_word",
-	//                     "search_analyzer": "ik_max_word",
-	//                     "include_in_all": "true"
-	//                 },
-	// 				"answer_content": {
-	//                     "type": "string",
-	//                     "analyzer": "ik_max_word",
-	//                     "search_analyzer": "ik_max_word",
-	//                     "include_in_all": "true"
-	//                 },
-	// 				"best_answer_content": {
-	//                     "type": "string",
-	//                     "analyzer": "ik_max_word",
-	//                     "search_analyzer": "ik_max_word",
-	//                     "include_in_all": "true"
-	//                 }
-	//             }
-	//         }
-	//     }
-	// }`}
-
-	//create index setting
-	// 	index := `{
-	//     "settings": {
-	//         "index": {
-	//             "analysis": {
-	//                 "analyzer": {
-	//                     "ik_pinyin_analyzer": {
-	//                         "type": "custom",
-	//                         "tokenizer": "ik_max_word",
-	//                         "filter": [
-	//                             "word_delimiter",
-	// 							"my_pinyin",
-	// 							"lowercase"
-	//                         ]
-	//                     }
-	//                 },
-	//                 "filter": {
-	//                     "my_pinyin": {
-	//                         "type": "pinyin",
-	//                         "first_letter": "prefix",
-	//                         "padding_char": " "
-	//                     }
-	//                 }
-	//             }
-	//         }
-	//     }
-	// }`
-	// 	http.Post(*es+"/yybdata/", "application/json", strings.NewReader(index))
-	//create mapping setting
-	// 	mapping := `{
-	//     "mappings": {
-	//         "app": {
-	//             "_all": {
-	//                 "enabled": false
-	//             },
-	//             "properties": {
-	//                 "title": {
-	//                     "type": "string",
-	//                     "analyzer": "ik_max_word",
-	//                     "search_analyzer": "ik_max_word",
-	//                     "include_in_all": "true"
-	//                 },
-	//                 "content": {
-	//                     "type": "string",
-	//                     "analyzer": "ik_max_word",
-	//                     "search_analyzer": "ik_max_word",
-	//                     "include_in_all": "true"
-	//                 }
-	//             }
-	//         }
-	//     }
-	// }`
-	// for _, setting := range settings {
-	// 	dest := *es + "/" + setting.indice + "/_mapping"
-	// 	http.Post(dest, "application/json", strings.NewReader(setting.mapping))
-	// 	log.Printf("###Create mapping for %v", setting.indice)
-	// }
 
 	var f *os.File
 	if f, err = os.Open(*file); err != nil {
@@ -286,43 +196,62 @@ func main() {
 				continue
 			}
 			//log.Printf("####%v", doc)
-			url, exist := doc["url"]
-			if !exist {
+			idstr := ""
+			url, urlexist := doc["url"]
+			if !urlexist {
 				log.Printf("No 'url' in line:%v", line)
 				buffer.Reset()
 				continue
 			}
-			indice := ""
+			urlstr := url.(string)
+			id, exist := doc["id"]
+			if !exist {
+				idstr = getMD5Hash(urlstr)
+			} else {
+				idstr = id.(string)
+			}
+
+			var cp DataSourceSetting
 			for _, v := range cfg.CP {
-				if strings.Contains(url.(string), v.Pattern) {
-					indice = v.Indice
-					if indice == "" {
-						indice = "default_data"
+				if strings.Contains(urlstr, v.Pattern) || v.Pattern == "*" {
+					cp = v
+					if doc["content"] == "" && cp.Type == "news" && cp.C2 == "article" {
+						cp.Type = "multimedia"
+						cp.C2 = "video"
 					}
-					doc["weight"] = v.Weight
-					doc["class"] = v.Class
+					doc["cw1"] = cp.CW1
+					doc["c1"] = cp.C1
+					doc["c2"] = cp.C2
+					//doc["class"] = v.Class
 					break
 				}
 			}
-			if indice == "" {
-				log.Printf("No match indice found for:%v", url)
+			if cp.Indice == "" {
+				log.Printf("No match type found for:%v", doc["url"])
 				buffer.Reset()
 				continue
 			}
+			delete(doc, "id")
 			b, _ := json.Marshal(doc)
 			buffer.Reset()
 			buffer.Write(b)
-			dest := *es + "/" + indice + "/app/" + getMD5Hash(url.(string))
+			dest := *es + "/" + cp.Indice + "/" + cp.Type + "/" + idstr
 			res, err1 := http.Post(dest, "application/json", &buffer)
 			if nil != err1 {
 				log.Printf("Failed to es:%v", err1)
 				return
 			}
-			log.Printf("###res:%v", indice)
-			if nil != res.Body {
-				io.Copy(ioutil.Discard, res.Body)
-				res.Body.Close()
+			if res.StatusCode >= 300 {
+				resb, _ := ioutil.ReadAll(res.Body)
+				log.Printf("##Recv error %d:%s", res.StatusCode, string(resb))
+			} else {
+				if nil != res.Body {
+					io.Copy(ioutil.Discard, res.Body)
+					res.Body.Close()
+				}
 			}
+			//log.Printf("###res:%v", etype)
+
 			processCount++
 			buffer.Reset()
 			if *count > 0 && processCount >= *count {
